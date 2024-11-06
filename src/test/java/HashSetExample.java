0074b1b9473899e569ddf8c8846e013f43a7eabf
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-06 20:55:08 +0400

    Task_2_Gocha

---
+++ src/test/java/HashSetExample.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        HashSet<Integer> numbers = new HashSet<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(20);
+        numbers.add(10);
+        System.out.println(numbers);
+
+        if (numbers.contains(20)) {
+            System.out.println("Set contains 20");
+
+        }
+
+        for (Integer num : numbers) {
+            System.out.println(num);
+
+        }
+    }
+}
