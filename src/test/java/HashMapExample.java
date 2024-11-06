f7716aab448d2b86ccb96281dfffc87d2ad72567
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-06 20:59:08 +0400

    Task_3_Gocha

---
+++ src/test/java/HashMapExample.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<>();
+
+        capitalCities.put("USA", "Washington D.C.");
+        capitalCities.put("France", "Paris");
+        capitalCities.put("Japan", "Tokyo");
+
+        System.out.println("Capital of France: " + capitalCities.get("France"));
+
+        capitalCities.remove("USA");
+
+        for (String country : capitalCities.keySet()) {
+            System.out.println(country + ": " + capitalCities.get(country));
+
+        }
+    }
+}
